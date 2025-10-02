4f8aeea420f7a5a085a923bef149aef2a0ac9ecb
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-03 02:54:54 +0600

    classone

--- class1.js
+++ class1.js
@@ -5,3 +5,10 @@ const district = `usa`;
 const thana = new String('uk')
 console.log(typeof thana)
 console.log( thana)
+const num = [3, 5, 2, 5, 6];
+console.log(num.length);
+const capital = 'Dh aka';
+console.log(capital.length)
+console.log(capital[1])
+// string is immutable.
+// array is mutable.
