a76315e0f44db464b33eefbeb2fa6d45ce7625ce
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-03 02:39:58 +0600

    first commit

---
+++ class1.js
@@ -0,0 +1,7 @@
+//introduction to string and string vs array
+const country = 'bangladesh';
+const division = 'dhaka';
+const district = `usa`;
+const thana = new String('uk')
+console.log(typeof thana)
+console.log( thana)
