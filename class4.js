418556ecab926aaaa95515f2ca59d0689c89e53e
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-05 03:30:55 +0600

    class5

--- class4.js
+++ class4.js
@@ -16,5 +16,5 @@ for(let i = 0; i < sentence.length; i++){
 }
 console.log(rev)
 
-const reversed = sentence.split('').reverse()
+const reversed = sentence.split('').reverse().join('')
 console.log(reversed)
---
+++ class5.js
@@ -0,0 +1,27 @@
+// introduction to objects properties and values
+//-------------> bellow those things are non primitive
+const age = 32;
+const name = 'jobbar kha';
+const isPassed = true;
+let isDeveloper;
+console.log(isPassed)
+const subjects = ['bangla', 'english', 'physics', 'math'];
+//object---------------> primitive
+const bottle = {
+    brand : 'apple',
+    price : 45,
+    color : 'white',
+    isClean : true
+}
+console.log(bottle)
+const subject  = {
+    name: 'biology',
+    teacher: 'rofik sir',
+    date: '30 feb',
+    chapter: ['first', 'second', 'third'],
+    exams : {
+        nam: 'final',
+        marks: 300,
+        isPass: true
+    }
+}
