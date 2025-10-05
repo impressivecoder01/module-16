72088aa442bc691e6f7a1b7bd84ef8f8eacb1c60
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-06 01:40:38 +0600

    class9

--- class9.js
+++ class9.js
@@ -37,3 +37,42 @@ const pencel = new Object ()
 console.log(pencel)
 const rubber = Object.create({})
 console.log(rubber)
+//------------------ practice
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+car[`passenger capacity`] = 5;
+console.log(car)
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+const pro = Object.keys(student)
+console.log(pro.length)
+
+let myObject = {
+
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+
+};
+for(let object in myObject){
+    console.log('keys:',object, '| type:', typeof object)
+}
+
+
+let js = 'javaScript is amazing';
+let count = 0;
+for(let j of js){
+    if(j === 'a'){
+        count = count + 1;
+    }
+}
+console.log(count)
